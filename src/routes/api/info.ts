4541a55247923f3550b76bf302cc9341d42a3598
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-11-27 19:12:17 +0000

    Cleaned up + added /github endpoint

--- src/routes/api/info.ts
+++ src/routes/api/info.ts
@@ -18,37 +18,40 @@ type redditData = {
 }
 
 export default function() {
-	const CovidHandler = new CacheHandler();
-	const RedditHandler = new CacheHandler();
 
 	/**
 	 * @API
 	 * /info/covid:
 	 *   get:
 	 *     description: Get COVID stats on a country or the world.
-	 *     tags: games
+	 *     tags: info
 	 *			parameters:
 	 *       - name: country
 	 *         description: The country to get COVID stats from
 	 *         required: false
 	 *         type: string
 	 */
+	const CovidHandler = new CacheHandler();
 	router.get('/covid', async (req, res) => {
 		const country = (req.query.country ?? '/all') as string;
+
 		let data = {};
 		if (CovidHandler.data.get(country)) {
-			console.log('Covid hit cache');
 			data = CovidHandler.data.get(country) as object;
 		} else {
-			console.log('Didn\t hit cache');
-			if (country != '/all') {
-				data = (await axios.get(`https://disease.sh/v3/covid-19/countries/${country}`)).data;
-				CovidHandler.data.set(country, data);
-			} else {
-				data = (await axios.get('https://disease.sh/v3/covid-19/all')).data;
-				CovidHandler.data.set('/all', data);
+			try {
+				if (country != '/all') {
+					data = (await axios.get(`https://disease.sh/v3/covid-19/countries/${country}`)).data;
+					CovidHandler.data.set(country, data);
+				} else {
+					data = (await axios.get('https://disease.sh/v3/covid-19/all')).data;
+					CovidHandler.data.set('/all', data);
+				}
+				CovidHandler._addData({ id: country, data: data });
+			} catch (err) {
+				console.log(err);
+				data = { error: 'Error' };
 			}
-			CovidHandler._addData({ id: country, data: data });
 		}
 
 		// Check cache first
@@ -60,13 +63,14 @@ export default function() {
 	 * /info/reddit:
 	 *   get:
 	 *     description: Get a post from a subreddit
-	 *     tags: games
+	 *     tags: info
 	 *			parameters:
 	 *       - name: sub
 	 *         description: The subreddit to get the post from.
 	 *         required: true
 	 *         type: string
 	 */
+	const RedditHandler = new CacheHandler();
 	router.get('/reddit', async (req, res) => {
 		if (!req.query.sub) return res.json({ error: CONSTANTS.REDDIT_MISSINGQUERY });
 		const sub = req.query.sub as string,
@@ -74,25 +78,28 @@ export default function() {
 
 		let sentData;
 		if (RedditHandler.data.get(`${sub}_${type}`)) {
-			console.log('Hit cache');
 			const data = RedditHandler.data.get(`${sub}_${type}`) as redditData;
 			sentData = new RedditPost(data.children[Utils.randomInteger(21)].data);
 		} else {
-			console.log('Didn\'t hit cache');
-			const dataRes = (await axios.get(`https://reddit.com/r/${sub}/${type}.json?limit=20`)).data;
+			try {
+				const dataRes = (await axios.get(`https://reddit.com/r/${sub}/${type}.json?limit=20`)).data;
 
-			// Make sure the subreddit has posts
-			const post = dataRes.data.children[Utils.randomInteger(21)];
-			const p = post?.data;
-			if (!p) {
-				res.json({ error: 'Subreddit does not exist or doesn\'t have any posts yet.' });
+				// Make sure the subreddit has posts
+				const post = dataRes.data.children[Utils.randomInteger(21)];
+				const p = post?.data;
+				if (!p) {
+					res.json({ error: 'Subreddit does not exist or doesn\'t have any posts yet.' });
+				}
+
+				// Return the data
+				RedditHandler._addData({ id: `${sub}_${type}`, data: dataRes.data });
+				sentData = new RedditPost(p);
+			} catch (err) {
+				console.log(err);
+				sentData = { error: 'Error' };
 			}
-
-			// Return the data
-			RedditHandler._addData({ id: `${sub}_${type}`, data: dataRes.data });
-			sentData = new RedditPost(p);
+			res.json(sentData);
 		}
-		res.json(sentData);
 	});
 
 	/**
@@ -100,36 +107,82 @@ export default function() {
 	 * /info/npm:
 	 *   get:
 	 *     description: Get information on a NPM package
-	 *     tags: games
+	 *     tags: info
 	 *			parameters:
 	 *       - name: package
 	 *         description: The name of the package
 	 *         required: true
 	 *         type: string
-	 */
+	*/
+	const NPMHandler = new CacheHandler();
 	router.get('/npm', async (req, res) => {
-		if (!req.query.package) return res.json({ error: 'No NPM package was provided in the query' });
+		const npmPackage = req.query.package as string;
+		if (!npmPackage) return res.json({ error: 'No NPM package was provided in the query' });
 
-		const data = (await axios.get(`https://registry.npmjs.com/${encodeURIComponent(req.query.package as string)}`)).data;
-		return res.json({
-			version: Object.keys(data.versions).reverse(),
-			description: data.description,
-			contributors: data.contributors,
-			homepage:  data.homepage,
-			keywords: data.keywords,
-			repository: data.repository,
-			bugs: data.bugs,
-			license: data.license,
-		});
+		let sentData = {};
+		if (NPMHandler.data.get(npmPackage)) {
+			sentData = NPMHandler.data.get(npmPackage) as object;
+		} else {
+			try {
+				const data = (await axios.get(`https://registry.npmjs.com/${encodeURIComponent(req.query.package as string)}`)).data;
+				const resp = {
+					version: Object.keys(data.versions).reverse(),
+					description: data.description,
+					contributors: data.contributors,
+					homepage:  data.homepage,
+					keywords: data.keywords,
+					repository: data.repository,
+					bugs: data.bugs,
+					license: data.license,
+				};
+				NPMHandler._addData({ id: npmPackage, data: resp });
+				sentData = resp;
+			} catch (err) {
+				console.log(err);
+				sentData = { error: 'Error' };
+			}
+		}
+		res.json(sentData);
 	});
 
-	type stuff = 'English' | 'Afrikaans'
+	/**
+	 * @API
+	 * /info/github:
+	 *   get:
+	 *     description: Get information on a Github repo
+	 *     tags: info
+	 *			parameters:
+	 *       - name: repo
+	 *         description: The name of the author and name of repo
+	 *         required: true
+	 *         type: string
+	*/
+	const GithubHandler = new CacheHandler();
+	router.get('/github', async (req, res) => {
+		const repo = req.query.repo as string;
+
+		let sentData = {};
+		if (GithubHandler.data.get(repo)) {
+			sentData = GithubHandler.data.get(repo) as object;
+		} else {
+			try {
+				const data = (await axios.get(`https://api.github.com/repos/${repo}`)).data;
+				GithubHandler._addData({ id: repo, data: data });
+				sentData = data;
+			} catch (err) {
+				console.log(err);
+				sentData = { error: 'Error' };
+			}
+		}
+		res.json(sentData);
+	});
+
 	/**
 	 * @API
 	 * /info/translate:
 	 *   get:
 	 *     description: Translate a message
-	 *     tags: games
+	 *     tags: info
 	 *			parameters:
 	 *       - name: text
 	 *         description: The text to translate
@@ -140,6 +193,7 @@ export default function() {
 	 *         required: false
 	 *         type: string
 	 */
+	type stuff = 'English' | 'Afrikaans'
 	router.get('/translate', async (req, res) => {
 		// Get text to translate
 		const text = req.query.text;
