d662cc3457a2386857795089c88f7d032c1801e4
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-06-04 00:57:18 +0100

    Updated footer to display new legal docs

--- frontend/src/components/navbar/footer.tsx
+++ frontend/src/components/navbar/footer.tsx
@@ -4,25 +4,22 @@ export default function Footer() {
 	return (
 		<div className="container">
 			<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-				<p className="col-md-4 mb-0 text-muted">© {new Date().getFullYear()} Company, Inc</p>
+				<p className="col-md-4 mb-0 text-muted">© {new Date().getFullYear()} Egglord</p>
 				<Link href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
 					<svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
 				</Link>
 				<ul className="nav col-md-4 justify-content-end">
 					<li className="nav-item">
-						<Link href="/" className="nav-link px-2 text-muted">Home</Link>
+						<Link href="/docs" className="nav-link px-2 text-muted">Features</Link>
 					</li>
 					<li className="nav-item">
-						<Link href="/" className="nav-link px-2 text-muted">Features</Link>
+						<Link href="/pricing" className="nav-link px-2 text-muted">Pricing</Link>
 					</li>
 					<li className="nav-item">
-						<Link href="/" className="nav-link px-2 text-muted">Pricing</Link>
+						<Link href="/privacy-policy" className="nav-link px-2 text-muted">Privacy Policy</Link>
 					</li>
 					<li className="nav-item">
-						<Link href="/" className="nav-link px-2 text-muted">FAQs</Link>
-					</li>
-					<li className="nav-item">
-						<Link href="/" className="nav-link px-2 text-muted">About</Link>
+						<Link href="/terms-and-service" className="nav-link px-2 text-muted">Terms & Service</Link>
 					</li>
 				</ul>
 			</footer>
