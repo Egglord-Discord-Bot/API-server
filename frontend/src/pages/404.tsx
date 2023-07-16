c73f37157e565085e22fb1571e6ea1ea016c3abf
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-07-16 13:28:23 +0100

    Added custom 404 error page

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,29 @@
+import Header from '../components/header';
+import Footer from '../components/navbar/footer';
+import Navbar from '../components/navbar/main';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+
+export default function FourZeroFour() {
+	const { data: session, status } = useSession();
+	if (status == 'loading') return null;
+
+	return (
+		<>
+			<Header />
+			<Navbar user={session?.user} />
+			<div className="page-wrap d-flex flex-row align-items-center" style={{ backgroundColor:'#f1f6fe', height: '84vh' }}>
+				<div className="container">
+					<div className="row justify-content-center align-middle">
+						<div className="col-md-12 text-center">
+							<span className="display-1 d-block">404</span>
+							<div className="mb-4 lead">The page you are looking for was not found.</div>
+							<Link href="/" className="btn btn-link">Back to Home</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
